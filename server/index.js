604b7e4ea6457a6b7a3c1b03a13496e091a41d69
nickbenz007 <nickbenz007@gmail.com>
2023-09-09 21:08:59 +0500

    InitialCommit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const colors = require("colors");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+const { graphqlHTTP } = require("express-graphql");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//  Connecting to MONGO DB
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(
+  port,
+  console.log(`
+Server is running on http://localhost:${port}
+`)
+);
